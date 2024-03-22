1616062ec5a962821cd858ddceb2f9c6a27f5103
apoorwagupta <apoorwa.gupta01@gmail.com>
2024-03-22 19:59:16 +0530

    navbar complete

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React from 'react' 
 
 const navIcons = [
     { src: '/assets/icons/search.svg', alt: 'search' },
@@ -22,13 +22,21 @@ const Navbar = () => {
                         Price<span className='text-primary'>Patrol</span>
                     </p>
                 </Link>
-                <div className='flex items-center gap-5'> </div>
+                <div className='flex items-center gap-5 '> 
                 {
                     navIcons.map((icon) => (
                         <Image
-                        key={icon}))
-                }
+                            key={icon.alt}
+                            src={icon.src}
+                            alt={icon.alt}
+                            width={28}
+                            height={28}
+                            className='object-contain'
+                        />))
+                    }
+                </div>
             </nav>
+
         </header>
   
   )
