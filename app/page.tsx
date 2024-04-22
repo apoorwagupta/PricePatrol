4beda3d55044614e41c9699347c903553bac63d9
apoorwagupta <apoorwa.gupta01@gmail.com>
2024-04-22 19:14:41 +0530

    navbar done, landing page started

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <section className='px-6 md:px-20 py-24 border-2 border-red-500'>
         <div className='flex max-xl:flex-col gap-16'>
-          <div className=' flex flex-col justify-center'>
+          <div className='flex flex-col justify-center'>
             <p className='small-text'>
               Smart Shopping starts Here:
               <Image
@@ -16,9 +16,24 @@ const Home = () => {
                 height={16}
               />
             </p>
+            <h1 className='head-text'>
+              Unleash The Power of 
+              <span className='text-primary'> PricePatrol
+              </span>
+            </h1>
+            <p className='mt-6'>Powerful, self-serve product and growth analytics to help you convert, engage, and retain more</p>
+           Searchbar
           </div>
+          HeroCarousel
         </div>
-
+      </section>
+      <section className='trending-section'>
+        <h2 className='section-text'>Trending</h2>
+        <div className='flex flex-wrap gap-x-8 gap-y-16'>
+          {['Apple Iphone 15', 'book', 'Sneakers'].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
       </section>
     </>
   )
