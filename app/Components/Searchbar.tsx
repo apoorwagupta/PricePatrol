91b1687aff6fa21fa0b9f1155bf81eed06e84ba8
apoorwagupta <apoorwa.gupta01@gmail.com>
2024-07-31 00:24:33 +0530

    searchbar

--- app/Components/Searchbar.tsx
+++ app/Components/Searchbar.tsx
@@ -1,9 +1,21 @@
+"use client"
 import React from 'react'
 
+// client side rendered component
+// interacitve component, use client
 const Searchbar = () => {
+  const handleSubmit=()=>{} 
   return (
-    <div>Searchbar</div>
+    <form
+      className='flex flex-wrap gap-4 mt-12'
+      onSubmit={handleSubmit}
+    >  
+      <input type='text' placeholder='Enter product link'
+      className='searchbar-input'/>
+      
+    </form>
   )
 }
 
 export default Searchbar
+
